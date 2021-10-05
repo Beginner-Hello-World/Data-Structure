7d8c49eea772f0468106ff89979a02e511c56506
Du shiwei <71075366+Beginner-Hello-World@users.noreply.github.com>
2021-10-05 22:55:20 +0800

    Update list.h

--- demo/List/Linked storage/list.h
+++ demo/List/Linked storage/list.h
@@ -1,1 +1,29 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<stdarg.h>
+/*
+* List线性表
+* 链式存储
+* 有链表头指针
+*/
+
+struct List{
+	int term;
+	int coeff;
+	struct List* next;
+};
+
+typedef struct List list;
+typedef struct List listItem;
+
+list* initList(void);
+void insertList(list* l, listItem* item,...);
+void destroyList(list* l);
+void clearList(list* l);
+int listEmpty(const list* l);
+int lengthList(const list* l);
+listItem getElem(const list* l, int location);
+int locateList(list* l, listItem value,int(*compare)(list*,listItem));
+listItem preListItem(list* l, listItem value);
+list* reverseList(list* L); 
